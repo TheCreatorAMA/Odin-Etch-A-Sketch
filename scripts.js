28f8a2df8a3f3d6d2c3dbd2309b19b121d03f0e3
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-08 09:29:46 -0700

    Add comments

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const mainGrid = document.querySelector('.main-grid');
 
+// This is here to simulate mouse "held" state
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 function generateGrid(rows, cols) {
+  // To generate grid using css-grid.
   mainGrid.style.cssText = `grid-template-rows: repeat(${rows}, 1fr);grid-template-columns: repeat(${cols}, 1fr);`;
 
   for (let item = 0; item < rows * cols; item++) {
@@ -19,6 +21,7 @@ function generateGrid(rows, cols) {
 }
 
 function changeColor(e) {
+  // only change background color of cell if mousedown has occured and mouse is in cell
   if (e.type === 'mouseover' && !mouseDown) return;
   e.target.style.cssText = 'background-color: black;';
 }

544fc7f1c6411c14fbec699e0c0310550dc0b15f
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-08 08:31:21 -0700

    Add javascript and html to make 16 x 16 grid using css grid

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 const testButton = document.querySelector('#test-button');
+const mainGrid = document.querySelector('.main-grid');
 
 testButton.addEventListener('click', () => {
   console.log('Working');
 });
+
+function generateGrid(rows, cols) {
+  mainGrid.style.cssText = `grid-template-rows: repeat(${rows}, 1fr);grid-template-columns: repeat(${cols}, 1fr);`;
+
+  for (let item = 0; item < rows * cols; item++) {
+    let newCell = document.createElement('div');
+    newCell.innerText = item + 1;
+    mainGrid.appendChild(newCell).className = 'grid-item';
+  }
+}
+
+generateGrid(16, 16);

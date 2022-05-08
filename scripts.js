ff544b7446a96bc81c8c1fe85739d988cd336587
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-08 09:04:50 -0700

    Add javascript to only color square when the mouse state is clicked and held

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
-const testButton = document.querySelector('#test-button');
 const mainGrid = document.querySelector('.main-grid');
 
-testButton.addEventListener('click', () => {
-  console.log('Working');
-});
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function generateGrid(rows, cols) {
   mainGrid.style.cssText = `grid-template-rows: repeat(${rows}, 1fr);grid-template-columns: repeat(${cols}, 1fr);`;
 
   for (let item = 0; item < rows * cols; item++) {
     let newCell = document.createElement('div');
-    newCell.innerText = item + 1;
+
+    // Event listener to
+    newCell.addEventListener('mouseover', changeColor);
+    newCell.addEventListener('mousedown', changeColor);
+    newCell.style.cssText = `width: ${1 / (rows * cols)};height: 1/(rows * cols)}%;`;
     mainGrid.appendChild(newCell).className = 'grid-item';
   }
+}
+
+function changeColor(e) {
+  if (e.type === 'mouseover' && !mouseDown) return;
+  e.target.style.cssText = 'background-color: black;';
 }
 
 generateGrid(16, 16);
